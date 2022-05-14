d6f57d83b39a461de63258222536af82dbeb8a8b
Wong Keng <kaira428@gmail.com>
2022-05-14 09:18:48 +0800

    Add useState in app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import {useState} from 'react';
+
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
 
 function App() {
+  const [showCart, setShowCart] = useState(false);
+  
   return (
     <CartProvider>
       <Header />
