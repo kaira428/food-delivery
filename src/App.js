26638ac61765784799ed93ffa2184f5c198a28d8
Wong Keng <kaira428@gmail.com>
2022-05-14 09:26:31 +0800

    Remove useState

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import {useState} from 'react';
-
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
 
 function App() {
-  const [showCart, setShowCart] = useState(false);
-  
   return (
     <CartProvider>
       <Header />
